acec7aecb32d7df1fb3cb4a4bf2e1f1936852e46
BabichaDevi <thbabicha@gmail.com>
2025-08-15 11:01:07 -0400

    demo

---
+++ app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/getResponse', (req, res) => {
+    console.log(req.body.question);
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+    model.generateContent(req.body.question).then(result=>{
+         console.log(result.response.text())
+         const response = result.response.text();
+         res.status(200).json({
+            response: response
+         })
+    })
+    .catch(err=>{
+        console.error(err);
+        res.status(500).json({
+            error: err
+        });
+    })
+}); 
+
+export default app;
