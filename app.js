acd05fd3eb352998f7bbfe0ae5c5fa7d5b62fb75
BabichaDevi <thbabicha@gmail.com>
2025-08-15 13:07:15 -0400

    demo

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/getResponse', (req, res) => {
